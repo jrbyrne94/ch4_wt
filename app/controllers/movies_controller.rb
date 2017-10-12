4d25ff7258888978a725e1561da203b16467a32a
Jonathan Byrne <jbyrne6@uncc.edu>
2017-10-12 01:35:23 +0000

    Finished 4.7

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -9,16 +9,23 @@ class MoviesController < ApplicationController
     # will render app/views/movies/show.html.haml by default
   end
   
+  def new
+    @movie = Movie.new
+    # default: render 'new' template
+  end 
+  
+  def create
+    #@movie = Movie.create!(params[:movie]) #old way
+    @movie = Movie.create!(movie_params)  # new way
+    flash[:notice] = "#{@movie.title} was successfully created."
+    redirect_to movies_path
+  end
+  
   # add below all other methods
   private
 
   def movie_params
     params.require(:movie).permit(:title, :rating, :description, :release_date)
   end
-  
-  def new
-    @movie = Movie.new
-    # default: render 'new' template
-  end 
   
 end
